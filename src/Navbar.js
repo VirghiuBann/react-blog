250bb0ad2f82bc68cb7d3d1bde93304e396bf45a
virghiu <virghiu@gmail.com>
2021-08-26 09:58:09 -0400

    router links

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
     <div className='navbar'>
       <h2>Blog</h2>
       <div className='links'>
-        <a href='/'>Home</a>
-        <a href='/create'>New Blog</a>
+        <Link to='/'>Home</Link>
+        <Link to='/create'>New Blog</Link>
       </div>
     </div>
   );
