cd5d89f118c298d53fcf59e103b9ea18e92e357e
virghiu <virghiu@gmail.com>
2021-08-25 23:18:43 -0400

    handling fetching error

--- src/Home.js
+++ src/Home.js
@@ -4,6 +4,7 @@ import BlogList from './BlogList';
 function Home() {
   const [blogs, setBlogs] = useState(null);
   const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   const handleDelete = (id) => {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
@@ -12,16 +13,25 @@ function Home() {
 
   useEffect(() => {
     async function fetchData() {
-      const resp = await fetch('http://localhost:8000/blogs');
-      const data = await resp.json();
-      setBlogs(data);
-      setIsPending(false);
+      try {
+        const resp = await fetch('http://localhost:8000/blogs');
+        if (!resp.ok) {
+          throw new Error('Fetching data failed');
+        }
+        const data = await resp.json();
+        setBlogs(data);
+        setIsPending(false);
+      } catch (error) {
+        setIsPending(false);
+        setError(error.message);
+      }
     }
     fetchData();
   }, []);
 
   return (
     <div className='home'>
+      {error && <div>{error}</div>}
       {isPending && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title={'All Blogs'} />}
     </div>
