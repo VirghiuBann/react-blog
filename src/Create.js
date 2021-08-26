b8a1a2768657bd37176b71c750df382699f9272f
virghiu <virghiu@gmail.com>
2021-08-26 13:23:32 -0400

    controlled inputs forms

--- src/Create.js
+++ src/Create.js
@@ -1,9 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Create() {
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [author, setAuthor] = useState('');
+
   return (
-    <div>
+    <div className='create'>
       <h2>Add new BLog</h2>
+      <form>
+        <label>Title:</label>
+        <input
+          type='text'
+          required
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+        />
+
+        <label>Body:</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => {
+            setBody(e.target.value);
+          }}
+        ></textarea>
+
+        <label>Author:</label>
+        <select
+          value={author}
+          onChange={(e) => {
+            setAuthor(e.target.value);
+          }}
+        >
+          <option value='mario'>mario</option>
+          <option value='yoshi'>yoshi</option>
+        </select>
+        <button>Add Blog</button>
+      </form>
     </div>
   );
 }
