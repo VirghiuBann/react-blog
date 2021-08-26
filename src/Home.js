21bf6df33cf71bf7c639d85dbaefc76537bdae28
virghiu <virghiu@gmail.com>
2021-08-25 23:03:20 -0400

    conditional rendering

--- src/Home.js
+++ src/Home.js
@@ -3,6 +3,7 @@ import BlogList from './BlogList';
 
 function Home() {
   const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
 
   const handleDelete = (id) => {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
@@ -13,14 +14,15 @@ function Home() {
     async function fetchData() {
       const resp = await fetch('http://localhost:8000/blogs');
       const data = await resp.json();
-      console.log(data);
       setBlogs(data);
+      setIsPending(false);
     }
     fetchData();
   }, []);
 
   return (
     <div className='home'>
+      {isPending && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title={'All Blogs'} />}
     </div>
   );
