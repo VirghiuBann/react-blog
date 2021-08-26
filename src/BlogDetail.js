902743bb1e487820939b8b882d3c33b2971b7bb8
virghiu <virghiu@gmail.com>
2021-08-26 13:48:27 -0400

    delete blog

--- src/BlogDetail.js
+++ src/BlogDetail.js
@@ -1,14 +1,24 @@
 import React from 'react';
-import { useParams } from 'react-router';
+import { useHistory, useParams } from 'react-router-dom';
 import useFetch from './useFetch';
 
 const BlogDetail = () => {
   const { id } = useParams();
+  const history = useHistory();
+
   const {
     data: blog,
     error,
     isPending,
   } = useFetch('http://localhost:8000/blogs/' + id);
+
+  const handleClick = () => {
+    fetch('http://localhost:8000/blogs/' + id, {
+      method: 'DELETE',
+    }).then(() => {
+      history.push('/');
+    });
+  };
 
   return (
     <div className='blog-details'>
@@ -19,6 +29,7 @@ const BlogDetail = () => {
           <h2>{blog.title}</h2>
           <p>Written by {blog.author}</p>
           <div>{blog.body}</div>
+          <button onClick={handleClick}>Delete</button>
         </article>
       )}
     </div>
